bc68a4eaea7549a054bc0275785afceb28edb356
Jorge Contreras <jorge.contreras789@gmail.com>
2020-09-05 12:49:13 -0400

    list github issues

--- src/Issues/index.tsx
+++ src/Issues/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router';
 import { IssuesMain } from './IssuesMain';
+import { ListIssues } from './ListIssues';
 
 const NewIssues = () => <>New Issues</>;
-const ListIssues = () => <>List Issues</>;
 
 export const Issues = () => {
   const match = useRouteMatch();
