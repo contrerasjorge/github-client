10a4faed1b695a982ca16ac262b77fb043cb536d
Jorge Contreras <jorge.contreras789@gmail.com>
2020-09-05 13:14:57 -0400

    create github issue

--- src/Issues/index.tsx
+++ src/Issues/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router';
 import { IssuesMain } from './IssuesMain';
 import { ListIssues } from './ListIssues';
-
-const NewIssues = () => <>New Issues</>;
+import { NewIssue } from './NewIssue';
 
 export const Issues = () => {
   const match = useRouteMatch();
@@ -11,7 +10,7 @@ export const Issues = () => {
   return (
     <Switch>
       <Route exact path={match.path} component={IssuesMain} />
-      <Route path={`${match.path}/new`} component={NewIssues} />
+      <Route path={`${match.path}/new`} component={NewIssue} />
       <Route path={`${match.path}/list`} component={ListIssues} />
     </Switch>
   );
