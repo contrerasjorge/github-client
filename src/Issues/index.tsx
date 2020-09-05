cf9d83174a287f6b5ecedacd9a4d13bd1a60821e
Jorge Contreras <jorge.contreras789@gmail.com>
2020-09-05 11:53:59 -0400

    created component to work with github issues

--- src/Issues/index.tsx
+++ src/Issues/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Panel } from '../Panel';
+import { Route, Switch, useRouteMatch } from 'react-router';
+import { IssuesMain } from './IssuesMain';
+
+const NewIssues = () => <>New Issues</>;
+const ListIssues = () => <>List Issues</>;
 
 export const Issues = () => {
+  const match = useRouteMatch();
+
   return (
-    <Panel height={10} top="25%" left="center">
-      <blessed-text left="center" bg="white" fg="black" content="Issues" />
-    </Panel>
+    <Switch>
+      <Route exact path={match.path} component={IssuesMain} />
+      <Route path={`${match.path}/new`} component={NewIssues} />
+      <Route path={`${match.path}/list`} component={ListIssues} />
+    </Switch>
   );
 };
